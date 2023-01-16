07f2aa9df91bfcb92dbd7d2addd56d41c2dd241c
imaei <imaei@foxmail.com>
2023-01-16 23:28:32 +0800

    feat: 平台主要模型

--- wangdejiang/src/main.go
+++ wangdejiang/src/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import "github.com/gin-gonic/gin"
+
+// 数据库初始化工作
+func init() {
+
+}
 
 func main() {
-	fmt.Println("hello everyday !!")
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // 监听并在 0.0.0.0:8080 上启动服务
 }
